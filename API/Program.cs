33c45b362e7184e80b42b2b9c4e0a4ee58ca2a81
Rajshree208 <rajshreerathour20@gmail.com>
2024-01-08 17:13:10 -0500

    Added client side code

--- API/Program.cs
+++ API/Program.cs
@@ -11,17 +11,11 @@ builder.Services.AddDbContext<DataContext>(opt=>{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
---
+++ client/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, HttpClientModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  title = 'Dating App';
+  users: any;
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:5139/api/users').subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete:()=> console.log('Request has completed')
+    })
+  }
+}
